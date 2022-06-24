936495519efff6eeeee94b7340644bb60712552a
Ulises Campodónico <uwulises@Ulisess-MacBook-Pro.local>
2022-06-24 01:51:31 -0400

    add function names

--- src/stepper_control/firmware/firmware.ino
+++ src/stepper_control/firmware/firmware.ino
@@ -1,91 +1,121 @@
-#include <AccelStepper.h>
-#include <FiniteStateMachine.h>
+#include <Servo.h>
+#include <Stepper.h>
+#include "MPU9250.h"
+
+MPU9250 mpu;
 // Definiciones para los pines de pulso y direccion
+#define pinservo 3
+#define pinrodillo 45
+Servo servokirby;   // nombre del servo es servokirby
 const int dirPin_1 = 4;
 const int stepPin_1 = 3;
 
-const int dirPin_2 = 6;
-const int stepPin_2 = 7;
+const int dirPin_2 = 15;
+const int stepPin_2 = 14;
+
+
 const int maxSpeed_stepper = 10000;
 const int accel = 10000;
-AccelStepper stepper_1(AccelStepper::DRIVER, stepPin_1, dirPin_1);
-AccelStepper stepper_2(AccelStepper::DRIVER, stepPin_2, dirPin_2);
+
+#define STEPS 400
+
+Stepper stepper_1(STEPS, dirPin_1, stepPin_1 );
+Stepper stepper_2(STEPS, dirPin_2, stepPin_2 );
+
 
 //for serial event
 String input_str = "";
 bool str_full = false;
-bool stop_button = false;
-State START = State(motor_start);
-State STOP = State(motor_stop);
-State FW = State(go_forward);
-State BW = State(go_backward);
-State TURN_R = State(turn_right);
-State TURN_L = State(turn_left);
-State READ_SENSORS = State(read_sensor);
-//initiate state machines
-FSM Motor = FSM(STOP);
-
 
 void setup() {
-  Serial.begin(9600);
-  stepper_1.setMaxSpeed(maxSpeed_stepper);
-  stepper_2.setMaxSpeed(maxSpeed_stepper);
-  stepper_1.setAcceleration(accel);
+  Serial.begin(115200);
+  Wire.begin();
+  delay(2000);
+
+  if (!mpu.setup(0x68)) {  // change to your own address
+    while (1) {
+      Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+      delay(5000);
+    }
+  }
+
+  //Serial.begin(9600);
+  servokirby.attach(pinservo);
+  pinMode(pinrodillo, OUTPUT);
   input_str.reserve(200);
   pinMode(LED_BUILTIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(20), stopp, CHANGE);
-}
-
-void motor_start()
-{
-  stepper_1.stop();
-  stepper_2.stop();
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
-
-}
-
-
-
-void motor_stop()
-{
-  stepper_1.stop();
-  stepper_2.stop();
-}
-
-void go_forward()
-{
   stepper_1.setSpeed(maxSpeed_stepper);
   stepper_2.setSpeed(maxSpeed_stepper);
-  while  (!stop_button) {
-  
-   stepper_1.move(100);
-   stepper_1.run();
-    //Serial.println("RUNNING");
-    //delay(10);
-  }
 }
 
-void go_backward()
-{
+
+
+void FW() {
+
+  stepper_1.step(10);
+  stepper_2.step(10);
+
 }
 
-void turn_right()
-{
+void BW() {
+
+  stepper_1.step(-10);
+  stepper_2.step(-10);
+
 }
 
-void turn_left()
-{
+void FW_to_pos(int distance) {
+
+  int pasos = distance * STEPS;
+  stepper_1.step(pasos);
+  stepper_2.step(pasos);
+  delay(10);
 }
 
-uint8_t read_sensor()
-{
+
+
+void levanta_servo()  // funcion que levanta el servo
+{ servokirby.write(180);
+  delay(30);
 }
 
-void stopp(){
-  stop_button=!stop_button;
+void baja_servo()  // funcion que baja el servo
+{ servokirby.write(0);
+  delay(30);
+}
+
+void prende_rodillo() {  // funcion que prende el rodillo
+  digitalWrite(pinrodillo, HIGH);
+}
+
+void apaga_rodillo() {  // funcion que apaga el rodillo
+  digitalWrite(pinrodillo, LOW);
+}
+
+
+void read_ultrasonic_1() {
+
+
+}
+
+
+void read_ultrasonic_2() {
+
+
+}
+
+void read_IMU() {
+
+}
+
+
+void print_roll_pitch_yaw() {
+  Serial.print("Yaw, Pitch, Roll: ");
+  Serial.print(mpu.getYaw(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getPitch(), 2);
+  Serial.print(", ");
+  Serial.println(mpu.getRoll(), 2);
 }
 
 void serialEvent() {
@@ -105,27 +135,6 @@ void serialEvent() {
 void loop() {
 
   if (str_full) {
-    Serial.println(input_str);
-    //set motor state depending on serial string
-    if (input_str.equals("START\n")) {
-      if (Motor.isInState(STOP)) {
-        Serial.println("Start!");
-        Motor.transitionTo(FW);
 
-      }
-    }
-    // clear the string:
-    input_str = "";
-    str_full = false;
-    Motor.update();
   }
-  else if(input_str.equals("STOP\n")){
-      
-        Motor.transitionTo(STOP);
-        }
-
-  else {
-    ;
-  }
-
 }
